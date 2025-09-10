da64c123d1de0d67e37030c9d45148e946005090
SamuelMoores <samueldmoores@gmail.com>
2025-09-09 17:20:06 -0700

    day and night and aladin

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject cam;
+    public float runSpeed;
+    public float turnSpeed;
+    Animator animator;
+    CharacterController controller;
+    bool freeze = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        controller.Move(Vector3.down * runSpeed * Time.deltaTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0.0f, vertical);
+        
+        if(moveDirection != Vector3.zero && CanMove())
+        {
+            moveDirection = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up) * moveDirection;
+            Quaternion toRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0.0f, moveDirection.z));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * 100.0f * Time.deltaTime);
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+
+        moveDirection.y = -9.8f;
+
+        moveDirection.Normalize();
+
+        if(CanMove())
+            controller.Move(moveDirection.normalized * runSpeed * Time.deltaTime);
+    }
+
+    bool CanMove()
+    {
+        return controller.isGrounded && !freeze;
+    }
+
+    public void Freeze()
+    {
+        freeze = true;
+    }
+
+    public void UnFreeze()
+    {
+        freeze = false;
+    }
+}
---
+++ Assets/Scripts/Statue.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class Statue : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject textObject;
+    public PlayerMovement player;
+    public int scene;
+    bool found = false;
+
+    private void Start()
+    {
+        text.SetActive(true);
+        Canvas.ForceUpdateCanvases();
+        text.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && found)
+        {
+            textObject.GetComponent<TextMeshProUGUI>().text = "Transporting to new world";
+            StartCoroutine(LoadLevel(3.0f));
+        }
+    }
+
+    IEnumerator LoadLevel(float duration)
+    {
+        float current = 0.0f;
+        player.Freeze();
+
+        while (current < duration)
+        {
+            current += Time.deltaTime;
+            textObject.GetComponent<TextMeshProUGUI>().text = "Transporting to new world";
+            yield return null;
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            text.SetActive(true);
+            found = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.SetActive(false);
+            found = false;
+        }
+    }
+}
